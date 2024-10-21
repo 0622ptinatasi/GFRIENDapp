3eecf1cfad6900f6e752d58f495f17a5c1d8a704
hw1 <tinatina62027@gmail.com>
2024-10-21 15:38:02 +0800

    Initial Commit

---
+++ HW2/HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW2App.swift
+//  HW2
+//
+//  Created by user12 on 2024/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
